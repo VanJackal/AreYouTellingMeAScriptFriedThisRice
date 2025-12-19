1fc70aa31c9c6bb36057a91813d9becf68282c7a
VanJackal <v@njackal.com>
2025-12-19 14:20:15 -0600

    initial renaming of stuff

--- src/client/java/com/njackal/friedrice/FriedRiceClient.java
+++ src/client/java/com/njackal/friedrice/FriedRiceClient.java
@@ -1,10 +1,11 @@
-package com.example;
+package com.njackal.friedrice;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class FriedRiceClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		FriedRice.LOGGER.info("FriedRice Client Init");
 	}
 }
